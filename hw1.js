a46c0f689e2b0fed89c2196ad771cdb8da68696f
Matt Canzoneri <matt.canzoneri@kellogg.northwestern.edu>
2018-02-05 18:25:28 -0600

    New approach directly querying arrays + updated weather

--- hw1.js
+++ hw1.js
@@ -15,22 +15,23 @@
 
 let weatherData = {
   current: {
-    temperature: 67,
-    conditions: "Sunny"
+    temperature: 12,
+    conditions: "snowy"
   },
   forecast: [
-    { temperature: 65, conditions: "Mostly Cloudy" },
-    { temperature: 70, conditions: "Partly Cloudy" }
+    { temperature: 19, conditions: "partly cloudy" },
+    { temperature: 23, conditions: "mostly cloudy" }
   ]
 }
 
-let currentTemp = weatherData.current.temperature;
-let currentCond = weatherData.current.conditions.toLowerCase();
-let tommTemp = weatherData.forecast[0].temperature;
-let tommCond = weatherData.forecast[0].conditions.toLowerCase();
-let afterTommTemp = weatherData.forecast[1].temperature;
-let afterTommCond = weatherData.forecast[1].conditions.toLowerCase();
+// let currentTemp = weatherData.current.temperature;
+// let currentCond = weatherData.current.conditions.toLowerCase();
+// let tommTemp = weatherData.forecast[0].temperature;
+// let tommCond = weatherData.forecast[0].conditions.toLowerCase();
+// let afterTommTemp = weatherData.forecast[1].temperature;
+// let afterTommCond = weatherData.forecast[1].conditions.toLowerCase();
 
-console.log("Currently it is " + currentTemp + " degrees and " + currentCond + "." + " Tomorrow it will be " + tommTemp + " and " + tommCond + "." + " The next day we will see " + afterTommCond + " skies, with a temperature of " + afterTommTemp + ".");
+// console.log("Currently it is " + currentTemp + " degrees and " + currentCond + "." + " Tomorrow it will be " + tommTemp + " and " + tommCond + "." + " The next day we will see " + afterTommCond + " skies, with a temperature of " + afterTommTemp + ".");
 // console.log("Tomorrow it will be " + tommTemp + " and " + tommCond + ".");
 // console.log("The next day we will see " + afterTommCond + " skies, with a temperature of " + afterTommTemp + ".");
+console.log("Currently it is " + weatherData.current.temperature + " degrees and " + weatherData.current.conditions + "." + " Tomorrow it will be " + weatherData.forecast[0].temperature + " degrees and " + weatherData.forecast[0].conditions + "." + " The next day we will see " + weatherData.forecast[1].conditions + " skies, with a temperature of " + weatherData.forecast[1].temperature + " degrees.");
